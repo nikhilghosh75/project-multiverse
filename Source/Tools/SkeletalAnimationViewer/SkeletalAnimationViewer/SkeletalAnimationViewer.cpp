5550bc691b38637f8313b9035cc7e3db920698e1
Nikhil Ghosh <nghosh606@gmail.com>
2024-10-21 21:38:23 -0400

    Added Image Viewer to the Skeletal Animation Viewer

--- Source/Tools/SkeletalAnimationViewer/SkeletalAnimationViewer/SkeletalAnimationViewer.cpp
+++ Source/Tools/SkeletalAnimationViewer/SkeletalAnimationViewer/SkeletalAnimationViewer.cpp
@@ -1,45 +1,155 @@
 #include "PhotoshopAPI/include/PhotoshopAPI.h"
 #include "Device.h"
 #include "Window.h"
+#include "Texture.h"
 #include "ImGuiDevice.h"
 #include "RenderManager.h"
 
 #include "imgui.h"
+#include "backends/imgui_impl_vulkan.h"
 
-void RenderLayer(PhotoshopAPI::Layer<uint8_t>* layer)
+#include <unordered_map>
+
+ImVec2 GetTextureDimensions(Texture* texture)
+{
+	return ImVec2(texture->GetTextureWidth(), texture->GetTextureHeight());
+}
+
+uint8_t* CombineChannelData(uint8_t* redChannel, uint8_t* greenChannel, uint8_t* blueChannel, uint8_t* alphaChannel, uint32_t width, uint32_t height)
+{
+	uint8_t* data = (uint8_t*)malloc(width * height * 4);
+
+	for (uint32_t i = 0; i < width * height; i++)
+	{
+		data[i * 4 + 0] = redChannel[i];
+		data[i * 4 + 1] = greenChannel[i];
+		data[i * 4 + 2] = blueChannel[i];
+		data[i * 4 + 3] = alphaChannel[i];
+	}
+
+	return data;
+}
+
+class LayerInfo
+{
+public:
+	Texture* texture;
+	VkDescriptorSet descriptorSet;
+};
+
+std::unordered_map<std::string, LayerInfo> layers;
+std::string currentLayer = "UNUSED";
+
+void RegisterLayer(PhotoshopAPI::Layer<uint8_t>* layer)
+{
+	PhotoshopAPI::ImageLayer<uint8_t>* imageLayer = dynamic_cast<PhotoshopAPI::ImageLayer<uint8_t>*>(layer);
+	PhotoshopAPI::GroupLayer<uint8_t>* groupLayer = dynamic_cast<PhotoshopAPI::GroupLayer<uint8_t>*>(layer);
+
+	if (imageLayer != nullptr)
+	{
+		// Split Image by Channel
+		std::vector<uint8_t> redChannel = imageLayer->getChannel(PhotoshopAPI::Enum::ChannelID::Red, true);
+		std::vector<uint8_t> greenChannel = imageLayer->getChannel(PhotoshopAPI::Enum::ChannelID::Green, true);
+		std::vector<uint8_t> blueChannel = imageLayer->getChannel(PhotoshopAPI::Enum::ChannelID::Blue, true);
+		std::vector<uint8_t> alphaChannel = imageLayer->getChannel(PhotoshopAPI::Enum::ChannelID::Alpha, true);
+
+		uint8_t* data = CombineChannelData(redChannel.data(), greenChannel.data(), blueChannel.data(), alphaChannel.data(), layer->m_Width, layer->m_Height);
+		Texture* texture = new Texture(data, layer->m_Width, layer->m_Height);
+
+		LayerInfo layerInfo;
+		layerInfo.texture = texture;
+		layerInfo.descriptorSet = ImGui_ImplVulkan_AddTexture(texture->GetSampler(), texture->GetImageView(), texture->GetImageLayout());
+
+		layers.insert({ layer->m_LayerName, layerInfo });
+	}
+	else if (groupLayer != nullptr)
+	{
+		for (int i = 0; i < groupLayer->m_Layers.size(); i++)
+		{
+			RegisterLayer(groupLayer->m_Layers[i].get());
+		}
+	}
+}
+
+void SetupLayerInfos(PhotoshopAPI::LayeredFile<uint8_t>* layeredFile)
+{
+	for (int i = 0; i < layeredFile->m_Layers.size(); i++)
+	{
+		RegisterLayer(layeredFile->m_Layers[i].get());
+	}
+}
+
+void RenderLayerHierarchy(PhotoshopAPI::Layer<uint8_t>* layer)
 {
 	PhotoshopAPI::GroupLayer<uint8_t>* groupLayer = dynamic_cast<PhotoshopAPI::GroupLayer<uint8_t>*>(layer);
 
+	ImGuiTreeNodeFlags flags = ImGuiTreeNodeFlags_OpenOnArrow;
+
+	if (layer->m_LayerName == currentLayer)
+	{
+		flags |= ImGuiTreeNodeFlags_Selected;
+	}
+
 	if (groupLayer != nullptr)
 	{
-		bool nodeOpen = ImGui::TreeNodeEx(layer->m_LayerName.c_str());
+		bool nodeOpen = ImGui::TreeNodeEx(layer->m_LayerName.c_str(), flags);
+		if (ImGui::IsItemClicked())
+		{
+			currentLayer = layer->m_LayerName;
+		}
 
 		if (nodeOpen)
 		{
 			for (int i = 0; i < groupLayer->m_Layers.size(); i++)
 			{
-				RenderLayer(groupLayer->m_Layers[i].get());
+				RenderLayerHierarchy(groupLayer->m_Layers[i].get());
 			}
 			ImGui::TreePop();
 		}
 	}
 	else
 	{
-		ImGuiTreeNodeFlags flags = ImGuiTreeNodeFlags_Leaf;
+		flags |= ImGuiTreeNodeFlags_Leaf;
 		ImGui::TreeNodeEx(layer->m_LayerName.c_str(), flags);
+
+		if (ImGui::IsItemClicked())
+		{
+			currentLayer = layer->m_LayerName;
+		}
+
 		ImGui::TreePop();
 	}
 }
 
-void RenderLayersWindow(PhotoshopAPI::LayeredFile<uint8_t>* layeredFile)
+void RenderLayersHierarchyWindow(PhotoshopAPI::LayeredFile<uint8_t>* layeredFile)
 {
-	ImGuiTreeNodeFlags flags = ImGuiTreeNodeFlags_None;
-
 	ImGui::Begin("Layers");
 	for (int i = 0; i < layeredFile->m_Layers.size(); i++)
 	{
-		RenderLayer(layeredFile->m_Layers[i].get());
+		RenderLayerHierarchy(layeredFile->m_Layers[i].get());
 	}
+	ImGui::End();
+}
+
+void RenderLayerImage()
+{
+	if (currentLayer == "UNUSED")
+	{
+		return;
+	}
+
+	auto foundLayer = layers.find(currentLayer);
+	if (foundLayer == layers.end())
+	{
+		return;
+	}
+
+	LayerInfo& currentLayerInfo = (*foundLayer).second;
+
+	ImGui::Begin("Layer Image");
+
+	ImGui::Image((ImTextureID)currentLayerInfo.descriptorSet, GetTextureDimensions(currentLayerInfo.texture));
+
 	ImGui::End();
 }
 
@@ -61,6 +171,8 @@ int main()
 	RenderManager renderingManager;
 	renderingManager.Setup();
 
+	SetupLayerInfos(layeredFile);
+
 	while (window.windowRunning)
 	{
 		window.Process();
@@ -68,7 +180,8 @@ int main()
 		device.StartFrame();
 		renderingManager.StartFrame();
 
-		RenderLayersWindow(layeredFile);
+		RenderLayersHierarchyWindow(layeredFile);
+		RenderLayerImage();
 
 		renderingManager.EndFrame();
 		device.EndFrame();
