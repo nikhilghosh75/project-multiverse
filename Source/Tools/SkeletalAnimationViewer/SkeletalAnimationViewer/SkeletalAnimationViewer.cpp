fcf2a36aaf006daa4cdc5432ed205a9c5dfdbbd3
Nikhil Ghosh <nghosh606@gmail.com>
2025-01-04 15:50:42 -0500

    Revamped Skeletal Animation Viewer

--- Source/Tools/SkeletalAnimationViewer/SkeletalAnimationViewer/SkeletalAnimationViewer.cpp
+++ Source/Tools/SkeletalAnimationViewer/SkeletalAnimationViewer/SkeletalAnimationViewer.cpp
@@ -1,8 +1,11 @@
 #include "PhotoshopAPI/include/PhotoshopAPI.h"
+
 #include "Device.h"
-#include "Window.h"
-#include "Texture.h"
 #include "ImGuiDevice.h"
+#include "Texture.h"
+#include "Window.h"
+
+#include "ImageRenderer.h"
 #include "RenderManager.h"
 
 #include "imgui.h"
@@ -35,10 +38,22 @@ class LayerInfo
 public:
 	Texture* texture;
 	VkDescriptorSet descriptorSet;
+	float centerX;
+	float centerY;
+	float width;
+	float height;
 };
 
 std::unordered_map<std::string, LayerInfo> layers;
 std::string currentLayer = "UNUSED";
+
+VkImage offscreenImage;
+VkImageView offscreenImageView;
+VkFramebuffer offscreenFramebuffer;
+VkDeviceMemory offscreenMemory;
+VkSampler offscreenSampler;
+VkRenderPass offscreenRenderPass;
+ImTextureID offscreenImguiTexture;
 
 void RegisterLayer(PhotoshopAPI::Layer<uint8_t>* layer)
 {
@@ -55,10 +70,15 @@ void RegisterLayer(PhotoshopAPI::Layer<uint8_t>* layer)
 
 		uint8_t* data = CombineChannelData(redChannel.data(), greenChannel.data(), blueChannel.data(), alphaChannel.data(), layer->m_Width, layer->m_Height);
 		Texture* texture = new Texture(data, layer->m_Width, layer->m_Height);
+		texture->TransitionLayout(VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL);
 
 		LayerInfo layerInfo;
 		layerInfo.texture = texture;
 		layerInfo.descriptorSet = ImGui_ImplVulkan_AddTexture(texture->GetSampler(), texture->GetImageView(), texture->GetImageLayout());
+		layerInfo.centerX = imageLayer->m_CenterX;
+		layerInfo.centerY = imageLayer->m_CenterY;
+		layerInfo.width = imageLayer->m_Width;
+		layerInfo.height = imageLayer->m_Height;
 
 		layers.insert({ layer->m_LayerName, layerInfo });
 	}
@@ -153,6 +173,192 @@ void RenderLayerImage()
 	ImGui::End();
 }
 
+uint32_t FindMemoryType(uint32_t typeFilter, VkMemoryPropertyFlags properties)
+{
+	VkPhysicalDeviceMemoryProperties memoryProperties;
+	vkGetPhysicalDeviceMemoryProperties(Device::Get()->GetPhysicalDevice(), &memoryProperties);
+
+	for (uint32_t i = 0; i < memoryProperties.memoryTypeCount; i++)
+	{
+		if ((typeFilter & (1 << i)) && (memoryProperties.memoryTypes[i].propertyFlags & properties) == properties)
+		{
+			return i;
+		}
+	}
+
+	return 0;
+}
+
+void SetupMainView()
+{
+	unsigned int width = 5000;
+	unsigned int height = 5000;
+
+	// Create image
+	VkImageCreateInfo imageInfo{};
+	imageInfo.sType = VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO;
+	imageInfo.imageType = VK_IMAGE_TYPE_2D;
+	imageInfo.format = VK_FORMAT_B8G8R8A8_SRGB; 
+	imageInfo.extent = { width, height, 1 };
+	imageInfo.mipLevels = 1;
+	imageInfo.arrayLayers = 1;
+	imageInfo.samples = VK_SAMPLE_COUNT_1_BIT;
+	imageInfo.tiling = VK_IMAGE_TILING_OPTIMAL;
+	imageInfo.usage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT | VK_IMAGE_USAGE_SAMPLED_BIT;
+	imageInfo.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED; 
+
+	vkCreateImage(Device::Get()->GetVulkanDevice(), &imageInfo, nullptr, &offscreenImage);
+
+	// Allocate and bind memory
+	VkMemoryRequirements memRequirements;
+	vkGetImageMemoryRequirements(Device::Get()->GetVulkanDevice(), offscreenImage, &memRequirements);
+
+	VkMemoryAllocateInfo allocInfo{};
+	allocInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
+	allocInfo.allocationSize = memRequirements.size;
+	allocInfo.memoryTypeIndex = FindMemoryType(memRequirements.memoryTypeBits, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT);
+
+	vkAllocateMemory(Device::Get()->GetVulkanDevice(), &allocInfo, nullptr, &offscreenMemory);
+	vkBindImageMemory(Device::Get()->GetVulkanDevice(), offscreenImage, offscreenMemory, 0);
+
+	// Create image view
+	VkImageViewCreateInfo viewInfo{};
+	viewInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+	viewInfo.image = offscreenImage;
+	viewInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+	viewInfo.format = VK_FORMAT_B8G8R8A8_SRGB;
+	viewInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+	viewInfo.subresourceRange.baseMipLevel = 0;
+	viewInfo.subresourceRange.levelCount = 1;
+	viewInfo.subresourceRange.baseArrayLayer = 0;
+	viewInfo.subresourceRange.layerCount = 1;
+
+	vkCreateImageView(Device::Get()->GetVulkanDevice(), &viewInfo, nullptr, &offscreenImageView);
+
+	// Create Render Pass
+	VkAttachmentDescription colorAttachment{};
+	colorAttachment.format = Device::Get()->GetSwapChainFormat();
+	colorAttachment.samples = VK_SAMPLE_COUNT_1_BIT;
+	colorAttachment.loadOp = VK_ATTACHMENT_LOAD_OP_LOAD;
+	colorAttachment.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
+	colorAttachment.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
+	colorAttachment.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+	colorAttachment.initialLayout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+	colorAttachment.finalLayout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+
+	VkAttachmentReference colorAttachmentRef{};
+	colorAttachmentRef.attachment = 0;
+	colorAttachmentRef.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+
+	VkSubpassDescription subpass{};
+	subpass.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS;
+	subpass.colorAttachmentCount = 1;
+	subpass.pColorAttachments = &colorAttachmentRef;
+
+	VkSubpassDependency dependency{};
+	dependency.srcSubpass = VK_SUBPASS_EXTERNAL;
+	dependency.dstSubpass = 0;
+	dependency.srcStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT;
+	dependency.srcAccessMask = 0;
+	dependency.dstStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT;
+	dependency.dstAccessMask = VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT;
+
+	VkRenderPassCreateInfo renderPassInfo{};
+	renderPassInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
+	renderPassInfo.attachmentCount = 1;
+	renderPassInfo.pAttachments = &colorAttachment;
+	renderPassInfo.subpassCount = 1;
+	renderPassInfo.pSubpasses = &subpass;
+	renderPassInfo.dependencyCount = 1;
+	renderPassInfo.pDependencies = &dependency;
+
+	if (vkCreateRenderPass(Device::Get()->GetVulkanDevice(), &renderPassInfo, nullptr, &offscreenRenderPass) != VK_SUCCESS)
+	{
+		// TODO: Output the following error code
+		// "Vulkan, failed to create render pass"
+		exit(0);
+	}
+
+	// Create framebuffer
+	VkFramebufferCreateInfo framebufferInfo{};
+	framebufferInfo.sType = VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO;
+	framebufferInfo.renderPass = offscreenRenderPass;
+	framebufferInfo.attachmentCount = 1;
+	framebufferInfo.pAttachments = &offscreenImageView;
+	framebufferInfo.width = width;
+	framebufferInfo.height = height;
+	framebufferInfo.layers = 1;
+
+	vkCreateFramebuffer(Device::Get()->GetVulkanDevice(), &framebufferInfo, nullptr, &offscreenFramebuffer);
+
+	VkSamplerCreateInfo samplerInfo{};
+	samplerInfo.sType = VK_STRUCTURE_TYPE_SAMPLER_CREATE_INFO;
+	samplerInfo.magFilter = VK_FILTER_LINEAR; // Use linear filtering for magnification
+	samplerInfo.minFilter = VK_FILTER_LINEAR; // Use linear filtering for minification
+	samplerInfo.addressModeU = VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE; // Clamp texture coordinates
+	samplerInfo.addressModeV = VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE;
+	samplerInfo.addressModeW = VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE;
+	samplerInfo.anisotropyEnable = VK_TRUE; // Enable anisotropic filtering if supported
+	samplerInfo.maxAnisotropy = 16.0f; // Adjust as needed; check device limits
+	samplerInfo.borderColor = VK_BORDER_COLOR_INT_OPAQUE_BLACK; // Border color for out-of-range coordinates
+	samplerInfo.unnormalizedCoordinates = VK_FALSE; // Use normalized texture coordinates (0.0 to 1.0)
+	samplerInfo.compareEnable = VK_FALSE; // Disable comparison for shadow maps
+	samplerInfo.compareOp = VK_COMPARE_OP_ALWAYS;
+	samplerInfo.mipmapMode = VK_SAMPLER_MIPMAP_MODE_LINEAR; // Use linear interpolation for mipmaps
+	samplerInfo.mipLodBias = 0.0f; // No bias
+	samplerInfo.minLod = 0.0f; // Minimum level of detail
+	samplerInfo.maxLod = 0.0f; // Maximum level of detail (set to 0 for no mipmapping)
+
+	vkCreateSampler(Device::Get()->GetVulkanDevice(), &samplerInfo, nullptr, &offscreenSampler);
+
+	Device::Get()->TransitionImageLayout(offscreenImage, VK_FORMAT_R8G8B8A8_UNORM, VK_IMAGE_LAYOUT_UNDEFINED, VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL);
+
+	offscreenImguiTexture = ImGui_ImplVulkan_AddTexture(offscreenSampler, offscreenImageView, VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL);
+}
+
+void RenderMainView()
+{
+	Device::Get()->TransitionImageLayout(offscreenImage, VK_FORMAT_R8G8B8A8_UNORM,
+		VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL, VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL);
+
+	int halfWidth = 2500;
+	int halfHeight = 2500;
+	unsigned int width = 5000;
+	unsigned int height = 5000;
+
+	RenderPass renderPass;
+	renderPass.extents = { width, height };
+	renderPass.framebuffer = offscreenFramebuffer;
+	renderPass.pass = offscreenRenderPass;
+
+	Device::Get()->SetOverrideRenderPass(renderPass);
+
+	// Render all of the images in the file one at a time
+	for (auto& layer : layers)
+	{
+		Rect rect(halfHeight + layer.second.centerY + layer.second.height, halfHeight + layer.second.centerY,
+			halfWidth + layer.second.centerX, halfWidth + layer.second.centerX + layer.second.width);
+
+		rect.top /= height;
+		rect.bottom /= height;
+		rect.left /= width;
+		rect.right /= width;
+
+		ImageRenderer::Get()->AddImage(layer.second.texture, rect);
+	}
+
+	Device::Get()->ClearOverrideRenderPass();
+
+	Device::Get()->TransitionImageLayout(offscreenImage, VK_FORMAT_R8G8B8A8_UNORM,
+		VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL, VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL);
+
+	ImGui::Begin("Main View");
+
+	ImGui::Image(offscreenImguiTexture, ImVec2(400, 400));
+
+	ImGui::End();
+}
+
 int main()
 {
 	PhotoshopAPI::File inputFile = PhotoshopAPI::File("C:/Users/debgh/OneDrive/Documents/Unity Projects/Dreamwillow/Assets/Art/Characters/Player/PlayerHorizSprite.psb");
@@ -172,6 +378,7 @@ int main()
 	renderingManager.Setup();
 
 	SetupLayerInfos(layeredFile);
+	SetupMainView();
 
 	while (window.windowRunning)
 	{
@@ -180,6 +387,7 @@ int main()
 		device.StartFrame();
 		renderingManager.StartFrame();
 
+		RenderMainView();
 		RenderLayersHierarchyWindow(layeredFile);
 		RenderLayerImage();
 
