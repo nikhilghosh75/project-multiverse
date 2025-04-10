203e5a5b4498a8248f123da34a2160759a6461ab
Nikhil Ghosh <nghosh606@gmail.com>
2025-04-09 21:43:21 -0400

    Fixed function and line macros

--- Source/Utils/AssertUtils.h
+++ Source/Utils/AssertUtils.h
@@ -3,7 +3,7 @@
 #include "ErrorManager.h"
 
 #if _DEBUG
-#define ASSERT(x) if (!(x)) { ErrorManager::Get()->ReportError(ErrorSeverity::Severe, "_##func", "", 0, #x); }
+#define ASSERT(x) if (!(x)) { ErrorManager::Get()->ReportError(ErrorSeverity::Severe, __FUNCTION__, "Assert", __LINE__, #x); }
 #else
 #define ASSERT(x);
 #endif // if _DEBUG
