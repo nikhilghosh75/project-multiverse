f50379efa5f6dd96fb56ea1583dff2c558a520c1
Nikhil Ghosh <nghosh606@gmail.com>
2025-07-21 22:26:24 -0400

    Added isCounterClockwise option to RenderPipeline

--- Source/Renderer/RenderPipeline.cpp
+++ Source/Renderer/RenderPipeline.cpp
@@ -62,6 +62,11 @@ void RenderPipeline::SetPolygonMode(VkPolygonMode mode)
     polygonMode = mode;
 }
 
+void RenderPipeline::SetFrontFace(bool _isCounterClockwise)
+{
+    isCounterClockwise = _isCounterClockwise;
+}
+
 void RenderPipeline::Create()
 {
     VkDevice vulkanDevice = Device::Get()->GetVulkanDevice();
@@ -121,7 +126,7 @@ void RenderPipeline::Create()
     rasterizer.polygonMode = polygonMode;
     rasterizer.lineWidth = 1.0f;
     rasterizer.cullMode = VK_CULL_MODE_BACK_BIT;
-    rasterizer.frontFace = VK_FRONT_FACE_CLOCKWISE;
+    rasterizer.frontFace = isCounterClockwise ? VK_FRONT_FACE_COUNTER_CLOCKWISE : VK_FRONT_FACE_CLOCKWISE;
     rasterizer.depthBiasEnable = VK_FALSE;
 
     // Setup multisampling (for antialiasing)
